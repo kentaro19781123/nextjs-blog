4ed76ba66c6178149c0bc530652f90a0b1011562
kentaro19781123 <kens19781123@gmail.com>
2021-03-21 18:29:43 +0900

    変数修正

--- pages/micro.js
+++ pages/micro.js
@@ -4,9 +4,9 @@ export default function micro({ news }) {
   return (
     <div>
       <ul>
-        {news.map(news => (
-          <li key={news.id}>
-            {news.title}
+        {news.map(item => (
+          <li key={item.id}>
+            {item.title}
           </li>
         ))}
       </ul>
@@ -18,7 +18,7 @@ export default function micro({ news }) {
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async () => {
   const key = {
-    headers: {'X-API-KEY': process.env.API_KEY},
+    headers: {'X-API-KEY': 'fddd9db3-2020-431a-98d8-ef85285b45c3'},
   };
   const data = await fetch('https://kentaro.microcms.io/api/v1/news', key)
     .then(res => res.json())
